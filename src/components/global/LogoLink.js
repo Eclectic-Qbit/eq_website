2ef3af3c3f0f11225d1dc152f4f46a566f723049
Enrypase <pasettoenrico02@gmail.com>
2023-05-15 00:09:16 +0200

    feat: made design mobile friendly. Removed not ready parts

--- src/components/global/LogoLink.js
+++ src/components/global/LogoLink.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function LogoLink({ children }) {
+export default function LogoLink({ children, href }) {
   return (
     <Link
-      href="https://instagram.com/eclecticqbit?igshid=YmMyMTA2M2Y="
+      href={href}
       target="_blank"
       className="bg-white rounded-full w-8 p-2 scale-100 hover:scale-125 transition"
     >
--- src/components/index/WhatWeDoSecond.js
+++ src/components/index/WhatWeDoSecond.js
@@ -1,12 +1,15 @@
+import { H9 } from "../text/Headers";
+import { P1 } from "../text/Paragraphs";
+
 export default function WhatWeDoSecond() {
   return (
     <div className="relative flex justify-center">
       <div className="flex flex-wrap gap-3 w-[60%] max-w-[130rem] lowercase">
         <div className="grid gap-2">
-          <h2 className="text-orange text-2xl">
+          <H9 className="text-orange text-2xl">
             👾 Gamification and Engagement: power up and network around 🤝
-          </h2>
-          <h2>
+          </H9>
+          <P1>
             but we don&apos;t stop there! we&apos;re also gamification masters
             we&apos;ll make your project jump higher, run faster, and defeat
             enemies to level up in the web3 world with QUESTS & CHALLENGES will
@@ -14,13 +17,13 @@ export default function WhatWeDoSecond() {
             and become stars players our strong network, will connect you with
             the right communities and resources to reach a state of
             superposition, interacting in ways that defy classical web3
-          </h2>
+          </P1>
         </div>
         <div className="grid gap-2">
-          <h2 className="text-orange text-2xl">
+          <H9 className="text-orange text-2xl">
             🍄 Grey area: techie Wizards and State-of-the-Art Tools 🚀
-          </h2>
-          <h2>
+          </H9>
+          <P1>
             we&apos;re not just about fun and games - we&apos;re also serious
             about the technical side of web3 Whether you&apos;re a blockchain
             newbie or a DeFi pro, our technical wizards are ready to help you
@@ -31,13 +34,13 @@ export default function WhatWeDoSecond() {
             we&apos;re not afraid to roll up our sleeves and get our hands dirty
             with smart contracts, decentralized storage, and all the other nerdy
             bits and bobs that power the decentralized web 🎮🚀🕹️🌐
-          </h2>
+          </P1>
         </div>
         <div className="grid gap-2">
-          <h2 className="text-orange text-2xl">
+          <H9 className="text-orange text-2xl">
             🌍 ReFi heads: we like it sustainable 🐢
-          </h2>
-          <h2>
+          </H9>
+          <P1>
             we have a deep appreciation for our planet, we value life with
             gratitude, and we also believe that society has been organized in a
             very unbalanced and harmful way still, we&apos;re positive thinker
@@ -45,7 +48,7 @@ export default function WhatWeDoSecond() {
             way to go from protocols and partnerships, to swags and irl&apos;s
             food, our commitment to taking the most sustainable path is
             unwavering 🌿💪🌐
-          </h2>
+          </P1>
         </div>
       </div>
     </div>
