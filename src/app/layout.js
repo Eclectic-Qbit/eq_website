aac16f3fa7008796a5253d1e4fdaccaba6f53a85
Enrypase <pasettoenrico02@gmail.com>
2023-05-16 18:22:11 +0200

    fix: default html language metadata is now "en". It won't be modified even if the language is changed by the user for NextJS client-side limitations

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,16 @@
-"use client";
-
 import Menu from "@/components/global/Menu";
 import "./globals.css";
 import LoadingAnimation from "@/components/animations/LoadingAnimation";
-import { useEffect, useState } from "react";
-import settings from "@/frontendSettings";
 import LanguageContext from "@/contexts/LanguageContext";
 
-const metadata = {
+export const metadata = {
   title: "Barrio Buidlers",
   description: "Interdipendence is the new indipendence",
 };
-export { metadata };
 
 export default function RootLayout({ children }) {
-  const [lang, setLang] = useState(settings.defaultLang);
-  useEffect(() => {
-    const userLang = localStorage.getItem("lang");
-    lang && lang !== userLang && setLang(userLang);
-  }, [lang]);
   return (
-    <html lang={lang} className="min-h-screen min-w-screen bg-black text-white">
+    <html lang="en" className="min-h-screen min-w-screen bg-black text-white">
       <body>
         <LanguageContext.Provider value={{ lang, setLang }}>
           <LoadingAnimation />
