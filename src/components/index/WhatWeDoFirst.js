749bcbdbd2803481057b5b7dcc014ff38acf00a7
Enrypase <pasettoenrico02@gmail.com>
2023-05-17 02:12:37 +0200

    fix: minor fixed for mobile view

--- src/components/index/WhatWeDoFirst.js
+++ src/components/index/WhatWeDoFirst.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { H2, H3, H4, H9 } from "../text/Headers";
-import { P1, P2, P4 } from "../text/Paragraphs";
+import { P1, P2, P3, P4 } from "../text/Paragraphs";
 
 export default function WhatWeDoFirst() {
   return (
@@ -10,12 +10,12 @@ export default function WhatWeDoFirst() {
         <div className="flex flex-wrap gap-8 mt-[5%] mx-[5%]">
           <div className="flex flex-col justify-center gap-9 max-w-2xl xl:max-w-4xl">
             <div className="font-extrabold uppercase text-purple w-full">
-              <H3 translationPath="whatWeDo/title/p1" />
-              <H2 translationPath="whatWeDo/title/p2" />
+              <H4 translationPath="whatWeDo/title/p1" />
+              <H3 translationPath="whatWeDo/title/p2" />
             </div>
             <div className="flex flex-wrap gap-3 lowercase w-full">
               <div className="grid gap-2">
-                <P2 translationPath="whatWeDo/h2" className="text-orange" />
+                <P3 translationPath="whatWeDo/h2" className="text-orange" />
                 <P4 translationPath="whatWeDo/p2" />
               </div>
             </div>
