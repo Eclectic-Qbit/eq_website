2cb4475f4f253c5e488a54e7b2525cdbb3fd91e0
Enrypase <pasettoenrico02@gmail.com>
2023-05-18 13:33:09 +0200

    fix: refractored general pages. Update testing page

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import "./globals.css";
+import LanguageProvider from "@/components/global/LanguageProvider";
+
+export const metadata = {
+  title: "Barrio Buidlers",
+  description: "Interdipendence is the new indipendence",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="min-h-screen min-w-screen bg-black text-white">
+      <body>
+        <LanguageProvider>{children}</LanguageProvider>
+      </body>
+    </html>
+  );
+}
