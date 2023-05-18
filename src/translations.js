8d3a2c5872a9c9c2f5fa8c41f1840e04fd09fde3
Enrypase <pasettoenrico02@gmail.com>
2023-05-18 16:04:38 +0200

    fix: adapted translations to <span> instead of strings

--- src/translations.js
+++ src/translations.js
@@ -1,20 +1,30 @@
 const translations = {
   menu: {
-    home: { en: "home", it: "home" },
-    brojects: { en: "brojects", it: "proggetti" },
-    collabs: { en: "collabs", it: "collaborazioni" },
-    team: { en: "team", it: "team" },
-    contacts: { en: "contacts", it: "contatti" },
+    home: { en: <span>home</span>, it: <span>home</span> },
+    brojects: { en: <span>brojects</span>, it: <span>proggetti</span> },
+    collabs: { en: <span>collabs</span>, it: <span>collaborazioni</span> },
+    team: { en: <span>team</span>, it: <span>team</span> },
+    contacts: { en: <span>contacts</span>, it: <span>contatti</span> },
   },
   eclecticQbit: {
-    title: { p1: { en: "eclectic" }, p2: { en: "qbit" } },
-    h1: { en: "interdependence is the new independence" },
+    title: { p1: { en: <span>eclectic</span> }, p2: { en: <span>qbit</span> } },
+    h1: { en: <span>interdependence is the new independence</span> },
     p1: {
-      en: "\"eclectic qbit, it's all about bringing the modus operandi of qbits, into web3, with a it's a me twist with nature as our main inspiration, just like fungi, we exchange nutrients and other resources within & beyond web3 space\"",
+      en: (
+        <span>
+          &quot;eclectic qbit, it&apos;s all about bringing the modus operandi
+          of qbits, into web3, with a it&apos;s a me twist with nature as our
+          main inspiration, just like fungi, we exchange nutrients and other
+          resources within & beyond web3 space&quot;
+        </span>
+      ),
     },
   },
   whatWeDo: {
-    title: { p1: { en: "this is" }, p2: { en: "what we do" } },
+    title: {
+      p1: { en: <span>this is</span> },
+      p2: { en: <span>what we do</span> },
+    },
 
     h2: {
       en: (
@@ -35,85 +45,165 @@ const translations = {
       ),
     },
     h3: {
-      en: "👾 gamification and engagement: power up and network around 🤝",
+      en: (
+        <span>
+          👾 gamification and engagement: power up and network around 🤝
+        </span>
+      ),
     },
     p3: {
       en: (
         <span>
-          but we don't stop there! we're also gamification masters we'll make
-          your project jump higher, run faster, and defeat enemies to level up
-          in the web3 world with quests & challenges will incentivize
-          engagement, making your troops stand out from the crowd and become
-          stars players. <br /> our strong network, will connect you with the
-          right communities and resources to reach a state of superposition,
-          interacting in ways that defy classical web3
+          but we don&apos;t stop there! we&apos;re also gamification masters
+          we&apos;ll make your project jump higher, run faster, and defeat
+          enemies to level up in the web3 world with quests & challenges will
+          incentivize engagement, making your troops stand out from the crowd
+          and become stars players. <br /> our strong network, will connect you
+          with the right communities and resources to reach a state of
+          superposition, interacting in ways that defy classical web3
         </span>
       ),
     },
-    h4: { en: "🍄 grey area: techie wizards and state-of-the-art tools 🚀" },
+    h4: {
+      en: (
+        <span>🍄 grey area: techie wizards and state-of-the-art tools 🚀</span>
+      ),
+    },
     p4: {
       en: (
         <span>
-          we're not just about fun and games - we're also serious about the
-          technical side of web3 whether you're a blockchain newbie or a defi
-          pro, our technical wizards are ready to help you navigate the complex
-          web of protocols, platforms, and technologies that make up the
-          decentralized web revolution we're into our. <br /> squad of techie
-          troublemakers is armed with the latest tools and frameworks to help
-          you smash your tech goals out of the park we're not afraid to roll up
-          our sleeves and get our hands dirty with smart contracts,
-          decentralized storage, and all the other nerdy bits and bobs that
-          power the decentralized web 🎮🚀🕹️🌐
+          we&apos;re not just about fun and games - we&apos;re also serious
+          about the technical side of web3 whether you&apos;re a blockchain
+          newbie or a defi pro, our technical wizards are ready to help you
+          navigate the complex web of protocols, platforms, and technologies
+          that make up the decentralized web revolution we&apos;re into our.{" "}
+          <br /> squad of techie troublemakers is armed with the latest tools
+          and frameworks to help you smash your tech goals out of the park
+          we&apos;re not afraid to roll up our sleeves and get our hands dirty
+          with smart contracts, decentralized storage, and all the other nerdy
+          bits and bobs that power the decentralized web 🎮🚀🕹️🌐
         </span>
       ),
     },
-    h5: { en: "🌍 refi heads: we like it sustainable 🐢" },
+    h5: { en: <span>🌍 refi heads: we like it sustainable 🐢</span> },
     p5: {
-      en: "we have a deep appreciation for our planet, we value life with gratitude, and we also believe that society has been organized in a very unbalanced and harmful way still, we're positive thinker and curios researcher,and the boyscout in us always finds the refi way to go from protocols and partnerships, to swags and irl's food, our commitment to taking the most sustainable path is unwavering 🌿💪🌐",
+      en: (
+        <span>
+          we have a deep appreciation for our planet, we value life with
+          gratitude, and we also believe that society has been organized in a
+          very unbalanced and harmful way still, we&apos;re positive thinker and
+          curios researcher,and the boyscout in us always finds the refi way to
+          go from protocols and partnerships, to swags and irl&apos;s food, our
+          commitment to taking the most sustainable path is unwavering 🌿💪🌐
+        </span>
+      ),
     },
   },
   howWeDo: {
-    title: { p1: { en: "this is" }, p2: { en: "how we do it" } },
-    h1: { en: "💬 warp pipes" },
+    title: {
+      p1: { en: <span>this is</span> },
+      p2: { en: <span>how we do it</span> },
+    },
+    h1: { en: <span>💬 warp pipes</span> },
     p1: {
-      en: "we build a discord server that provides a space for eclectic Koopas to connect, share ideas, and provide feedback on your project",
-      it: "costruiamo un server discord che mette a disposizione uno spazio per gli eclectic Koopas per connettersi, condividere idee e darsi feedback sui progetti",
+      en: (
+        <span>
+          we build a discord server that provides a space for eclectic Koopas to
+          connect, share ideas, and provide feedback on your project
+        </span>
+      ),
+      it: (
+        <span>
+          costruiamo un server discord che mette a disposizione uno spazio per
+          gli eclectic Koopas per connettersi, condividere idee e darsi feedback
+          sui progetti
+        </span>
+      ),
     },
-    h2: { en: "🎉 contests" },
+    h2: { en: <span>🎉 contests</span> },
     p2: {
-      en: "we organize regular contests that allow eclectic Koopas to show off their skills and earn prizes",
+      en: (
+        <span>
+          we organize regular contests that allow eclectic Koopas to show off
+          their skills and earn prizes
+        </span>
+      ),
     },
-    h3: { en: "🍄 power-ups" },
+    h3: { en: <span>🍄 power-ups</span> },
     p3: {
-      en: "our team provides guidance and resources to help your community members level up and reach new milestones",
+      en: (
+        <span>
+          our team provides guidance and resources to help your community
+          members level up and reach new milestones
+        </span>
+      ),
     },
-    h4: { en: "🏆 high scores" },
+    h4: { en: <span>🏆 high scores</span> },
     p4: {
-      en: "we track and celebrate the achievements of our eclectic koopas, from the number of piece of art created to the amount of poaps collected & beyond",
+      en: (
+        <span>
+          we track and celebrate the achievements of our eclectic koopas, from
+          the number of piece of art created to the amount of poaps collected &
+          beyond
+        </span>
+      ),
     },
-    h5: { en: "🧑‍🤝‍🧑 partnerships" },
+    h5: { en: <span>🧑‍🤝‍🧑 partnerships</span> },
     p5: {
-      en: "we help our eclectic Koopas form partnerships with other web3 projects & beyond, expanding their reach and building new connections",
+      en: (
+        <span>
+          we help our eclectic Koopas form partnerships with other web3 projects
+          & beyond, expanding their reach and building new connections
+        </span>
+      ),
     },
-    h6: { en: "🎨 fan art contests" },
+    h6: { en: <span>🎨 fan art contests</span> },
     p6: {
-      en: "our eclectic Koopas can showcase their creativity and love for your project through fan art contests, where winners are rewarded with nft and prizes",
+      en: (
+        <span>
+          our eclectic Koopas can showcase their creativity and love for your
+          project through fan art contests, where winners are rewarded with nft
+          and prizes
+        </span>
+      ),
     },
-    h7: { en: "🕹️ game nights" },
+    h7: { en: <span>🕹️ game nights</span> },
     p7: {
-      en: "we organize fun game nights where eclectic Koopas can play&earn games, compete with each other, and build strong relationships",
+      en: (
+        <span>
+          we organize fun game nights where eclectic Koopas can play&earn games,
+          compete with each other, and build strong relationships
+        </span>
+      ),
     },
-    h8: { en: "🌟 launch parties" },
+    h8: { en: <span>🌟 launch parties</span> },
     p8: {
-      en: "just like the beginning of a new adventure, we celebrate the launch of your project with a party that gets everyone excited and ready to jump in",
+      en: (
+        <span>
+          just like the beginning of a new adventure, we celebrate the launch of
+          your project with a party that gets everyone excited and ready to jump
+          in
+        </span>
+      ),
     },
-    h9: { en: "🌎 global events" },
+    h9: { en: <span>🌎 global events</span> },
     p9: {
-      en: "our team coordinates global irl events, as online voice channels & activities to bring together eclectic Koopas from around the world",
+      en: (
+        <span>
+          our team coordinates global irl events, as online voice channels &
+          activities to bring together eclectic Koopas from around the world
+        </span>
+      ),
     },
-    h10: { en: "🔮 quantum predictions" },
+    h10: { en: <span>🔮 quantum predictions</span> },
     p10: {
-      en: "inspired by quantum mechanics's principles, we'll create prediction markets where eclectic Koopas can use their knowledge and intuition to predict the future of the project",
+      en: (
+        <span>
+          inspired by quantum mechanics&apos;s principles, we&apos;ll create
+          prediction markets where eclectic Koopas can use their knowledge and
+          intuition to predict the future of the project
+        </span>
+      ),
     },
   },
 };
