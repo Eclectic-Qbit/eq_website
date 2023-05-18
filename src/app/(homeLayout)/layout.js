d08d8957e7a79553595bf10c5a626b72d5a5b790
Enrypase <pasettoenrico02@gmail.com>
2023-05-18 13:33:09 +0200

    fix: refractored general pages. Update testing page

--- src/app/(homeLayout)/layout.js
+++ src/app/(homeLayout)/layout.js
@@ -1,7 +1,5 @@
 import Menu from "@/components/global/Menu";
-import "../globals.css";
 import LoadingAnimation from "@/components/animations/LoadingAnimation";
-import LanguageProvider from "@/components/global/LanguageProvider";
 import LogoLink from "@/components/global/LogoLink";
 import {
   DiscordLogo,
@@ -10,40 +8,31 @@ import {
 } from "@/components/logos/FullLogo";
 import { InstagramLogo, TwitterLogo } from "@/components/logos/BorderLogo";
 
-export const metadata = {
-  title: "Barrio Buidlers",
-  description: "Interdipendence is the new indipendence",
-};
-
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="min-h-screen min-w-screen bg-black text-white">
-      <body>
-        <LanguageProvider>
-          <LoadingAnimation />
-          <Menu />
-          <div>
-            <div className="absolute z-10 top-[50%] -translate-y-[50%] right-[2%] flex flex-col gap-3">
-              <LogoLink href="https://discord.gg/n3gxVeqq">
-                <DiscordLogo fill={"black"} width={"100%"} height={"100%"} />
-              </LogoLink>
-              <LogoLink href="https://twitter.com/EclecticQbit">
-                <TwitterLogo fill={"black"} width={"100%"} height={"100%"} />
-              </LogoLink>
-              <LogoLink href="https://www.reddit.com/user/Eclecticqbit/">
-                <RedditLogo fill={"black"} width={"100%"} height={"100%"} />
-              </LogoLink>
-              <LogoLink href="https://instagram.com/eclecticqbit?igshid=YmMyMTA2M2Y=">
-                <InstagramLogo fill={"black"} width={"100%"} height={"100%"} />
-              </LogoLink>
-              <LogoLink href="https://t.me/eclecticqbit">
-                <TelegramLogo fill={"black"} width={"100%"} height={"100%"} />
-              </LogoLink>
-            </div>
-            <div className="mt-[17.5vh]">{children}</div>
-          </div>
-        </LanguageProvider>
-      </body>
-    </html>
+    <div>
+      <LoadingAnimation />
+      <Menu />
+      <div>
+        <div className="absolute z-10 top-[50%] -translate-y-[50%] right-[2%] flex flex-col gap-3">
+          <LogoLink href="https://discord.gg/n3gxVeqq">
+            <DiscordLogo fill={"black"} width={"100%"} height={"100%"} />
+          </LogoLink>
+          <LogoLink href="https://twitter.com/EclecticQbit">
+            <TwitterLogo fill={"black"} width={"100%"} height={"100%"} />
+          </LogoLink>
+          <LogoLink href="https://www.reddit.com/user/Eclecticqbit/">
+            <RedditLogo fill={"black"} width={"100%"} height={"100%"} />
+          </LogoLink>
+          <LogoLink href="https://instagram.com/eclecticqbit?igshid=YmMyMTA2M2Y=">
+            <InstagramLogo fill={"black"} width={"100%"} height={"100%"} />
+          </LogoLink>
+          <LogoLink href="https://t.me/eclecticqbit">
+            <TelegramLogo fill={"black"} width={"100%"} height={"100%"} />
+          </LogoLink>
+        </div>
+        <div className="mt-[17.5vh]">{children}</div>
+      </div>
+    </div>
   );
 }
--- src/app/testing/page.js
+++ src/app/testing/page.js
@@ -1,8 +1,86 @@
 "use client";
 
+import { MouseMoveGradient } from "@/components/animations/MouseMoveAnimations";
 import FirstCanvas from "@/components/backgrounds/FirstCanvas";
+import WhatWeDoFirst from "@/components/index/WhatWeDoFirst";
+import WhatWeDoSecond from "@/components/index/WhatWeDoSecond";
+import { H4 } from "@/components/text/Headers";
+import { P1 } from "@/components/text/Paragraphs";
 import { useEffect, useRef } from "react";
 
 export default function Testing() {
-  return <FirstCanvas />;
+  return (
+    <div className="realtive w-full h-full">
+      <div className="absolute top-0 left-0">
+        <FirstCanvas />
+      </div>
+      <WhatWeDoFirst />
+      <div className="relative flex flex-wrap justify-center gap-6">
+        <div>
+          <H4 className="text-orange font-extrabold">HELLO</H4>
+          <H4 className="text-orange font-bold">HELLO</H4>
+          <H4 className="text-orange">HELLO</H4>
+          <P1 className="text-orange font-extrabold">hello</P1>
+          <P1 className="text-orange font-bolder">hello</P1>
+          <P1 className="text-orange">hello</P1>
+        </div>
+        <div>
+          <H4 className="text-purple font-extrabold">HELLO</H4>
+          <H4 className="text-purple font-bold">HELLO</H4>
+          <H4 className="text-purple">HELLO</H4>
+          <P1 className="text-purple font-extrabold">hello</P1>
+          <P1 className="text-purple font-bolder">hello</P1>
+          <P1 className="text-purple">hello</P1>
+        </div>
+        <div>
+          <H4 className="text-pink font-extrabold">HELLO</H4>
+          <H4 className="text-pink font-bold">HELLO</H4>
+          <H4 className="text-pink">HELLO</H4>
+          <P1 className="text-pink font-extrabold">hello</P1>
+          <P1 className="text-pink font-bolder">hello</P1>
+          <P1 className="text-pink">hello</P1>
+        </div>
+        <div>
+          <H4 className="text-green font-extrabold">HELLO</H4>
+          <H4 className="text-green font-bold">HELLO</H4>
+          <H4 className="text-green">HELLO</H4>
+          <P1 className="text-green font-extrabold">hello</P1>
+          <P1 className="text-green font-bolder">hello</P1>
+          <P1 className="text-green">hello</P1>
+        </div>
+        <div>
+          <H4 className="text-blue font-extrabold">HELLO</H4>
+          <H4 className="text-blue font-bold">HELLO</H4>
+          <H4 className="text-blue">HELLO</H4>
+          <P1 className="text-blue font-extrabold">hello</P1>
+          <P1 className="text-blue font-bolder">hello</P1>
+          <P1 className="text-blue">hello</P1>
+        </div>
+        <div>
+          <H4 className="text-yellow font-extrabold">HELLO</H4>
+          <H4 className="text-yellow font-bold">HELLO</H4>
+          <H4 className="text-yellow">HELLO</H4>
+          <P1 className="text-yellow font-extrabold">hello</P1>
+          <P1 className="text-yellow font-bolder">hello</P1>
+          <P1 className="text-yellow">hello</P1>
+        </div>
+        <div>
+          <H4 className="text-white font-extrabold">HELLO</H4>
+          <H4 className="text-white font-bold">HELLO</H4>
+          <H4 className="text-white">HELLO</H4>
+          <P1 className="text-white font-extrabold">hello</P1>
+          <P1 className="text-white font-bolder">hello</P1>
+          <P1 className="text-white">hello</P1>
+        </div>
+        <MouseMoveGradient className="" from="#FF6600" to="#9500E9">
+          <H4 className="font-extrabold">HELLO</H4>
+          <H4 className=" font-bold">HELLO</H4>
+          <H4 className="">HELLO</H4>
+          <P1 className=" font-extrabold">hello</P1>
+          <P1 className=" font-bolder">hello</P1>
+          <P1 className="">hello</P1>
+        </MouseMoveGradient>
+      </div>
+    </div>
+  );
 }
