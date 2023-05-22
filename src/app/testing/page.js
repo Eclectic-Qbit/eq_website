cbaff574bf5efb377ba8b61c27a32c7328b839ec
Enrypase <pasettoenrico02@gmail.com>
2023-05-22 12:43:20 +0200

    feat: added starts reset when mouse stops and not on fixed timer. Stars generation is linear instead of square

--- src/app/testing/page.js
+++ src/app/testing/page.js
@@ -1,94 +1,13 @@
 "use client";
 
-/*
-
-<div className="relative flex flex-wrap justify-center gap-6">
-          <div>
-            <H4 className="text-orange font-extrabold">HELLO</H4>
-            <H4 className="text-orange font-bold">HELLO</H4>
-            <H4 className="text-orange">HELLO</H4>
-            <P1 className="text-orange font-extrabold">hello</P1>
-            <P1 className="text-orange font-bolder">hello</P1>
-            <P1 className="text-orange">hello</P1>
-          </div>
-          <div>
-            <H4 className="text-purple font-extrabold">HELLO</H4>
-            <H4 className="text-purple font-bold">HELLO</H4>
-            <H4 className="text-purple">HELLO</H4>
-            <P1 className="text-purple font-extrabold">hello</P1>
-            <P1 className="text-purple font-bolder">hello</P1>
-            <P1 className="text-purple">hello</P1>
-          </div>
-          <div>
-            <H4 className="text-pink font-extrabold">HELLO</H4>
-            <H4 className="text-pink font-bold">HELLO</H4>
-            <H4 className="text-pink">HELLO</H4>
-            <P1 className="text-pink font-extrabold">hello</P1>
-            <P1 className="text-pink font-bolder">hello</P1>
-            <P1 className="text-pink">hello</P1>
-          </div>
-          <div>
-            <H4 className="text-green font-extrabold">HELLO</H4>
-            <H4 className="text-green font-bold">HELLO</H4>
-            <H4 className="text-green">HELLO</H4>
-            <P1 className="text-green font-extrabold">hello</P1>
-            <P1 className="text-green font-bolder">hello</P1>
-            <P1 className="text-green">hello</P1>
-          </div>
-          <div>
-            <H4 className="text-blue font-extrabold">HELLO</H4>
-            <H4 className="text-blue font-bold">HELLO</H4>
-            <H4 className="text-blue">HELLO</H4>
-            <P1 className="text-blue font-extrabold">hello</P1>
-            <P1 className="text-blue font-bolder">hello</P1>
-            <P1 className="text-blue">hello</P1>
-          </div>
-          <div>
-            <H4 className="text-yellow font-extrabold">HELLO</H4>
-            <H4 className="text-yellow font-bold">HELLO</H4>
-            <H4 className="text-yellow">HELLO</H4>
-            <P1 className="text-yellow font-extrabold">hello</P1>
-            <P1 className="text-yellow font-bolder">hello</P1>
-            <P1 className="text-yellow">hello</P1>
-          </div>
-          <div>
-            <H4 className="text-white font-extrabold">HELLO</H4>
-            <H4 className="text-white font-bold">HELLO</H4>
-            <H4 className="text-white">HELLO</H4>
-            <P1 className="text-white font-extrabold">hello</P1>
-            <P1 className="text-white font-bolder">hello</P1>
-            <P1 className="text-white">hello</P1>
-          </div>
-          <MouseMoveGradient className="" from="#FF6600" to="#9500E9">
-            <H4 className="font-extrabold">HELLO</H4>
-            <H4 className=" font-bold">HELLO</H4>
-            <H4 className="">HELLO</H4>
-            <P1 className=" font-extrabold">hello</P1>
-            <P1 className=" font-bolder">hello</P1>
-            <P1 className="">hello</P1>
-          </MouseMoveGradient>
-        </div>
-
-*/
-
 import { useCallback, useEffect, useMemo, useRef } from "react";
-
-/*
-
-1) yellow: "#CCFF00",
-2) orange: "#FF6600",
-3) blue: "#00BFFF",
-4) green: "#00FF00",
-5) pink: "#FF007F",
-6) purple: "#9500E9",
-
-*/
 
 export default function Testing() {
   const canvasRef = useRef(null);
   const items = useRef([]);
   const lastTime = useRef(0);
-  const TIMEOUT = 10;
+  const timeouts = useRef([]);
+  const TIMEOUT = 20;
   const CIRCLE = 40;
   const colors = useMemo(() => {
     return ["#CCFF00", "#FF6600", "#00BFFF", "#00FF00", "#FF007F", "#9500E9"];
@@ -107,10 +26,64 @@ export default function Testing() {
     },
     []
   );
+  const createTimeout = useCallback(
+    (ctx, i, w, creationDate) => {
+      const newTimeout = setTimeout(() => {
+        drawCircle(
+          ctx,
+          items.current[i][0],
+          items.current[i][1],
+          w + 1,
+          1,
+          1,
+          0,
+          "black"
+        );
+        drawCircle(
+          ctx,
+          items.current[i][0],
+          items.current[i][1],
+          w,
+          1,
+          1,
+          0,
+          "white"
+        );
+        timeouts.current = timeouts.current.filter((el) => {
+          if (
+            el.params[0] !== ctx ||
+            el.params[1] !== i ||
+            el.params[2 !== w]
+          ) {
+            return el;
+          }
+        });
+      }, 5 * TIMEOUT + Date.now() - creationDate);
+      return newTimeout;
+    },
+    [drawCircle]
+  );
+  const posticipateTimeouts = useCallback(() => {
+    const newTimeouts = [];
+    timeouts.current.map((el, i) => {
+      clearTimeout(el.timeout);
+      newTimeouts.push({
+        timeout: createTimeout(
+          el.params[0],
+          el.params[1],
+          el.params[2],
+          el.params[3]
+        ),
+        params: [el.params[0], el.params[1], el.params[2], el.params[3]],
+      });
+    });
+    timeouts.current = newTimeouts;
+  }, [createTimeout]);
   const handleMouseMove = useCallback(
     (e, ctx) => {
       const date = Date.now();
       if (date - lastTime.current > TIMEOUT) {
+        posticipateTimeouts();
         const x = e.clientX;
         const y = e.clientY;
         for (let i = 0; i < items.current.length; i++) {
@@ -146,30 +119,10 @@ export default function Testing() {
                   0,
                   color
                 );
-                setTimeout(() => {
-                  drawCircle(
-                    ctx,
-                    items.current[i][0],
-                    items.current[i][1],
-                    w + 1,
-                    1,
-                    1,
-                    0,
-                    "black"
-                  );
-                  drawCircle(
-                    ctx,
-                    items.current[i][0],
-                    items.current[i][1],
-                    w,
-                    1,
-                    1,
-                    0,
-                    "white"
-                  );
-                }, 5000);
-                //items.current.splice(i, 1);
-                //items.current.push([x, y, w, date]);
+                timeouts.current.push({
+                  timeout: createTimeout(ctx, i, w, 0),
+                  params: [ctx, i, w, Date.now()],
+                });
               }
             }
           }
@@ -177,18 +130,21 @@ export default function Testing() {
         lastTime.current = Date.now();
       }
     },
-    [colors, drawCircle]
+    [colors, createTimeout, drawCircle, posticipateTimeouts]
   );
   useEffect(() => {
     canvasRef.current.width = canvasRef.current.parentNode.clientWidth;
     canvasRef.current.height = canvasRef.current.parentNode.clientHeight;
     const context = canvasRef.current.getContext("2d");
-    for (let i = 11; i < window.innerWidth - 11; i += 11) {
-      for (let j = 11; j < window.innerHeight - 11; j += 11) {
+    for (let i = 7; i < window.innerWidth - 7; i += 7) {
+      for (let j = 0; j < window.innerHeight / 15; j++) {
+        const randomY = Math.floor(
+          Math.random() * (window.innerHeight - 7 + 1) + 7
+        );
         const num = Math.floor(Math.random() * (1000 - 0 + 1) + 0);
         if (num > 950) {
-          drawCircle(context, i, j, num > 990 ? 2 : 1, 1, 1, 0, "white");
-          items.current.push([i, j, num > 985 ? 2 : 1, 0]);
+          drawCircle(context, i, randomY, num > 990 ? 2 : 1, 1, 1, 0, "white");
+          items.current.push([i, randomY, num > 985 ? 2 : 1, 0]);
         }
       }
     }
