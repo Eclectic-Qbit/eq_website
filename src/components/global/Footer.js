7444168e0af040ebe4557e6901a54876bb4e9f67
Enrypase <pasettoenrico02@gmail.com>
2023-05-31 11:00:46 +0200

    fix: minor UI changes

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -1,12 +1,52 @@
+import Link from "next/link";
+import { InstagramLogo, TwitterLogo } from "../logos/BorderLogo";
+import { DiscordLogo, RedditLogo, TelegramLogo } from "../logos/FullLogo";
 import { P3 } from "../text/Paragraphs";
 import CustomLink from "./CustomLink";
+import LogoLink from "./LogoLink";
 
 export default function Footer() {
   return (
-    <div className="flex w-full h-full items-center justify-center py-8 px-4 border-t-2 border-solid border-white">
-      <CustomLink href="/contacts">
-        <P3 translationPath="menu/contacts" />
-      </CustomLink>
+    <div className="px-4 pb-4 border-t-2 border-solid border-white">
+      <div className="grid grid-cols-3 w-full h-full gap-[5%] items-center text-center">
+        <div className="flex flex-col justify-start gap-2 scale-[0.8]">
+          <LogoLink href="https://discord.gg/n3gxVeqq">
+            <DiscordLogo fill={"black"} width={"100%"} height={"100%"} />
+          </LogoLink>
+          <LogoLink href="https://twitter.com/EclecticQbit">
+            <TwitterLogo fill={"black"} width={"100%"} height={"100%"} />
+          </LogoLink>
+          <LogoLink href="https://www.reddit.com/user/Eclecticqbit/">
+            <RedditLogo fill={"black"} width={"100%"} height={"100%"} />
+          </LogoLink>
+          <LogoLink href="https://instagram.com/eclecticqbit?igshid=YmMyMTA2M2Y=">
+            <InstagramLogo fill={"black"} width={"100%"} height={"100%"} />
+          </LogoLink>
+          <LogoLink href="https://t.me/eclecticqbit">
+            <TelegramLogo fill={"black"} width={"100%"} height={"100%"} />
+          </LogoLink>
+        </div>
+        <div className="flex flex-col justify-end gap-2">
+          <CustomLink href="/">
+            <P3 translationPath="menu/home" />
+          </CustomLink>
+          <CustomLink href="/brojects">
+            <P3 translationPath="menu/brojects" />
+          </CustomLink>
+          <CustomLink href="/collabs">
+            <P3 translationPath="menu/collabs" />
+          </CustomLink>
+          <CustomLink href="/squad">
+            <P3 translationPath="menu/squad" />
+          </CustomLink>
+        </div>
+        <P3 className="font-extrabold border-b-2 border-solid border-white uppercase w-max -rotate-90 sm:rotate-0 -translate-x-[25%] sm:translate-x-[0%]">
+          Eclectic Qbit
+        </P3>
+      </div>
+      <div className="text-center">
+        <P3>eclecticqbit@gmail.com</P3>
+      </div>
     </div>
   );
 }
