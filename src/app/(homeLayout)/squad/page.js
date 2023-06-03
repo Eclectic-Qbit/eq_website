456b8268bd8e680b5ef3ff3c50fbe4becd1c52ef
Enrypase <pasettoenrico02@gmail.com>
2023-06-03 17:29:03 +0200

    feat: added WritingEffect for Brojects. Created default pages for Collabs and Squad

---
+++ src/app/(homeLayout)/squad/page.js
@@ -0,0 +1,5 @@
+export default function Squad() {
+  return (
+    <div className="relative top-0 left-0 w-full h-full min-h-screen"></div>
+  );
+}
--- src/components/specific/brojects/StickySection.js
+++ src/components/specific/brojects/StickySection.js
@@ -1,3 +1,36 @@
-export default function StickySection({ children }) {
-  return <div className="sticky top-0 w-full h-full">{children}</div>;
+"use client";
+
+import ScrollContext from "@/contexts/ScrollContext";
+import { useContext, useEffect, useRef } from "react";
+
+export default function StickySection({
+  className,
+  children,
+  changeVisibility,
+  texts,
+}) {
+  const ref = useRef(null);
+  const wasVisible = useRef(false);
+  const { scroll } = useContext(ScrollContext);
+  useEffect(() => {
+    if (ref.current.getBoundingClientRect().y < window.innerHeight) {
+      if (!wasVisible.current) {
+        changeVisibility(texts.visible);
+        wasVisible.current = true;
+      }
+    } else if (wasVisible.current) {
+      changeVisibility(texts.invisible);
+      wasVisible.current = false;
+    }
+  }, [changeVisibility, scroll, texts.invisible, texts.visible]);
+  return (
+    <div
+      ref={ref}
+      className={`${
+        className ? className : ""
+      } sticky top-0 w-full h-full min-h-screen py-20`}
+    >
+      {children}
+    </div>
+  );
 }
