73377f84c6cbd1ad39941f3bda07f46d4237f8c6
Enrypase <pasettoenrico02@gmail.com>
2023-06-05 15:10:20 +0200

    fix: minor fixes

--- src/components/global/LoadingScreen.js
+++ src/components/global/LoadingScreen.js
@@ -1,30 +1,36 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import LoadingAnimation from "../animations/LoadingAnimation";
 import { H3, H4 } from "../text/Headers";
 
 export default function LoadingScreen() {
   const [forceFade, setForceFade] = useState(false);
+  const alreadyLoaded = useRef(false);
   function onFade() {
     sessionStorage.setItem("loaded", true);
   }
   useEffect(() => {
     const cookie = JSON.parse(sessionStorage.getItem("loaded"));
-    setForceFade(cookie);
+    setForceFade(!alreadyLoaded.current && cookie);
+    alreadyLoaded.current = true;
   }, []);
   return (
-    <LoadingAnimation
-      elements={[
-        <H4 key={0}>Interdependence</H4>,
-        <H3 key={1}>Is the new</H3>,
-        <H4 key={2}>Independence</H4>,
-      ]}
-      coeffs={[1, 1.5, 2.25]}
-      delay={2000}
-      className="z-30 fixed top-0 left-0 w-full h-full bg-black flex items-center justify-center transition-all duration-1000 ease-in text-black sm:text-white"
-      forceFade={forceFade}
-      onFade={onFade}
-    />
+    <div>
+      {!forceFade && (
+        <LoadingAnimation
+          elements={[
+            <H4 key={0}>Interdependence</H4>,
+            <H3 key={1}>Is the new</H3>,
+            <H4 key={2}>Independence</H4>,
+          ]}
+          coeffs={[1, 1.5, 2.25]}
+          delay={2000}
+          className="z-30 fixed top-0 left-0 w-full h-full bg-black flex items-center justify-center transition-all duration-1000 ease-in text-black sm:text-white"
+          forceFade={forceFade}
+          onFade={onFade}
+        />
+      )}
+    </div>
   );
 }
