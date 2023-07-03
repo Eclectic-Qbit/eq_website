e0edf62bbd82b533bd11d29bae6fde1342182ede
bitspresso <bitspresso@hotmail.com>
2023-07-03 22:43:00 +0000

    feat: create one-time-claim NFT pass

--- contracts/Pass.sol
+++ contracts/Pass.sol
@@ -1,7 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Pass is ERC721 {
-    constructor() ERC721("Pass, Collectable", "Pass") {
+contract Q_0 is ERC721, Ownable {
+    uint lastId = 1;
+    mapping(address => uint) usersPass;
 
+    constructor() ERC721("Quest #0 Pass", "Q_0") {}
+
+    function mint() public {
+        require(usersPass[msg.sender] == 0);
+
+        _safeMint(msg.sender, lastId);
+
+        unchecked {
+            ++lastId;
+        }
+    }
+
+    function hasMinted(address sender) public view returns (bool) {
+        return usersPass[sender] == 0;
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
     }
 }
