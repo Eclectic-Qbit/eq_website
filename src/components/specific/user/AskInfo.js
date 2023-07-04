d71feb72a7d0b7390e54c5f5e7ba1303a2e4d42d
Enrypase <pasettoenrico02@gmail.com>
2023-07-04 02:47:08 +0200

    feat: added approximated templated for final user page

--- src/components/specific/user/AskInfo.js
+++ src/components/specific/user/AskInfo.js
@@ -1,59 +1,116 @@
 "use client";
 
 import ConsoleEffect from "@/components/animations/ConsoleEffect";
-import { useRef, useState } from "react";
+import { P2 } from "@/components/text/Paragraphs";
+import { useCallback, useEffect, useRef, useState } from "react";
 
-export default function AskInfo({ title, placeholder, send, userInfo }) {
-  const [val, setVal] = useState("");
-  const requirements = useRef(
-    "Your username must be at least 3 chars long and must contain numbers, letters or symbols like $,&,.,_"
+export default function AskInfo({
+  title,
+  placeholder,
+  send,
+  descrPhrase,
+  bottomPhrase,
+  defaultValue,
+}) {
+  const DELTA = useRef(30);
+  const [hovering, setHovering] = useState(false);
+  const [val, setVal] = useState(defaultValue);
+  const [error, setError] = useState(null);
+  function callbackError() {
+    setError("Value not valid");
+  }
+  const handleSend = useCallback(
+    (ignore) => {
+      send(val, callbackError, () => setVal(""), ignore);
+    },
+    [send, val]
   );
+  useEffect(() => {
+    error && setVal("");
+  }, [error]);
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center gap-5">
       <ConsoleEffect
-        style={{ textShadow: "2px 2px 2px black", width: "max-content" }}
+        style={{ textShadow: "2px 2px 2px black", width: "100%" }}
         className="text-white"
         content={{ content: title, type: "raw" }}
         forceActive={true}
-        delta={40}
+        delta={DELTA.current}
       />
       <div className="flex items-center border-2 border-solid border-white w-max h-max py-2 px-4">
         <input
           className="bg-transparent cursor-none outline-none"
           type="text"
           placeholder={placeholder}
-          onChange={(e) => setVal(e.target.value)}
-          onKeyDown={(e) => console.log(e.key)}
-        />
-        <div className="text-white">=&gt;</div>
-      </div>
-      <ConsoleEffect
-        style={{ textShadow: "2px 2px 2px black", width: "max-content" }}
-        className="text-white"
-        content={{
-          content: requirements.current,
-          type: "raw",
-        }}
-        forceActive={true}
-        delta={40}
-        delay={title.length * 40 * 2}
-      />
-      <button
-        onClick={send("no")}
-        className="absolute bottom-4 left-[50%] -translate-x-[50%]"
-      >
-        <ConsoleEffect
-          style={{ textShadow: "2px 2px 2px black", width: "max-content" }}
-          className="text-white hover:underline"
-          content={{
-            content: `Or keep using your discord username (${userInfo.username})`,
-            type: "raw",
+          value={val}
+          onChange={(e) => {
+            setVal(e.target.value);
+            if (error) {
+              setError(null);
+            }
           }}
-          forceActive={true}
-          delta={40}
-          delay={requirements.current.length * 40 * 2}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSend();
+            }
+          }}
         />
-      </button>
+        <div onClick={handleSend} className="text-white">
+          =&gt;
+        </div>
+      </div>
+      <div>
+        {error && (
+          <div className="text-center">
+            <ConsoleEffect
+              style={{ textShadow: "2px 2px 2px black", width: "100%" }}
+              className="text-red-500"
+              content={{
+                content: error,
+                type: "raw",
+              }}
+              forceActive={true}
+              delta={DELTA.current}
+            />
+          </div>
+        )}
+      </div>
+      {descrPhrase && (
+        <div className="text-center max-w-[80%]">
+          <ConsoleEffect
+            style={{ textShadow: "2px 2px 2px black", width: "100%" }}
+            className="text-white"
+            content={{
+              content: descrPhrase,
+              type: "raw",
+            }}
+            forceActive={true}
+            delta={DELTA.current}
+            delay={title.length * DELTA.current * 2}
+          />
+        </div>
+      )}
+      {bottomPhrase && (
+        <button
+          onMouseEnter={() => setHovering(true)}
+          onMouseLeave={() => setHovering(false)}
+          onClick={() => send("no", null, null, true)}
+          className="absolute bottom-4 left-[50%] -translate-x-[50%] cursor-none"
+        >
+          <ConsoleEffect
+            style={{ textShadow: "2px 2px 2px black", width: "100%" }}
+            className="text-white hover:underline"
+            content={{
+              content: bottomPhrase,
+              type: "raw",
+            }}
+            spanStyling={hovering ? { textDecoration: "underline" } : {}}
+            forceActive={true}
+            delta={DELTA.current}
+            delay={descrPhrase ? descrPhrase.length * DELTA.current * 2 : 0}
+          />
+        </button>
+      )}
     </div>
   );
 }
