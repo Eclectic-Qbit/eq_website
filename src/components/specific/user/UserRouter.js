8d8e77690d901a1ee3d0931a5d6999837b119dd6
Enrypase <pasettoenrico02@gmail.com>
2023-07-04 15:15:19 +0200

    feat: added state between user data insertion

--- src/components/specific/user/UserRouter.js
+++ src/components/specific/user/UserRouter.js
@@ -2,12 +2,21 @@
 
 import { H3 } from "@/components/text/Headers";
 import ChosePFP from "./ChosePFP";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AskInfo from "./AskInfo";
 import FinalPage from "./FinalPage";
+import ImgTab from "../../../../public/images/team/4.png";
+import ImgEnry from "../../../../public/images/team/3.png";
+import ImgPipo from "../../../../public/images/team/6.png";
+import ImgAdi from "../../../../public/images/team/7.png";
+import ImgAxel from "../../../../public/images/team/1.png";
+import ImgAbra from "../../../../public/images/team/5.png";
+import ImgPari from "../../../../public/images/team/8.png";
+import ImgMilena from "../../../../public/images/team/10.png";
 
 export default function UserRouter({ userInfo }) {
-  const [pfp, setPfp] = useState(null);
+  const [page, setPage] = useState(0);
+  const [pfp, setPfp] = useState(-2);
   const [username, setUsername] = useState(null);
   const [city, setCity] = useState(null);
   function sendUsername(val, errorCallback, successCallback, ignore) {
@@ -17,12 +26,14 @@ export default function UserRouter({ userInfo }) {
           setUsername(val);
           successCallback();
           // Fetch
+          setPage(2);
           return;
         }
       }
       errorCallback();
     } else {
       setUsername("no");
+      setPage(2);
       successCallback();
     }
   }
@@ -33,15 +44,21 @@ export default function UserRouter({ userInfo }) {
     }
     setCity(val);
     successCallback();
+    setPage(3);
+  }
+  function sendPfp(val, errorCallback, successCallback, ignore) {
+    setPfp(val);
+    setPage(1);
   }
   return (
     <>
-      {userInfo.pfp || pfp ? (
-        userInfo.customUsername || username ? (
-          userInfo.city || city ? (
+      {page > 0 ? (
+        page > 1 ? (
+          page === 3 ? (
             <FinalPage />
           ) : (
             <AskInfo
+              key={"ask_city"}
               title={"What city are you in?"}
               placeholder={"Ospedaletto"}
               send={(val, callback, ignore) => sendCity(val, callback, ignore)}
@@ -49,10 +66,14 @@ export default function UserRouter({ userInfo }) {
                 "Feel free to share your (approximate) location! This will help us to understand more about our community. We might do an event in your city too! In case you're too shy, you can leave the field blank"
               }
               defaultValue=""
+              state={city !== "no" ? city : ""}
+              forcePage={(val) => setPage(val)}
+              page={2}
             />
           )
         ) : (
           <AskInfo
+            key={"username"}
             title={"Would you like to have a custom username?"}
             placeholder={userInfo.username}
             send={(val, callback, ignore) =>
@@ -63,10 +84,28 @@ export default function UserRouter({ userInfo }) {
             }
             bottomPhrase={`Or keep using your discord username (${userInfo.username})`}
             defaultValue=""
+            state={username !== "no" ? username : ""}
+            forcePage={(val) => setPage(val)}
+            page={1}
           />
         )
       ) : (
-        <ChosePFP userInfo={userInfo} setPfp={setPfp} />
+        <ChosePFP
+          userInfo={userInfo}
+          setPfp={sendPfp}
+          images={[
+            ImgTab,
+            ImgEnry,
+            ImgPipo,
+            ImgAdi,
+            ImgAxel,
+            ImgAbra,
+            ImgPari,
+            ImgMilena,
+          ]}
+          state={pfp >= -1 ? pfp : 0}
+          skipIntro={pfp >= -1}
+        />
       )}
     </>
   );
