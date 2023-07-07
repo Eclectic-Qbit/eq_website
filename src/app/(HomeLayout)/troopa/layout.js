5358a792b143a2dbb2921abf0ed6ed4814176399
Enrypase <pasettoenrico02@gmail.com>
2023-07-07 23:51:26 +0200

    debug: uploaded right layout

--- src/app/(HomeLayout)/troopa/layout.js
+++ src/app/(HomeLayout)/troopa/layout.js
@@ -1,38 +1,24 @@
-import CurrentPageProvider from "@/providers/CurrentPageProvider";
-import "./globals.css";
-import LanguageProvider from "@/providers/LanguageProvider";
-import MouseProvider from "@/providers/MouseProvider";
-import ScrollProvider from "@/providers/ScrollProvider";
-import DebouncedResizeProvider from "@/providers/DebouncedResizeProvider";
-import ResizeProvider from "@/providers/ResizeProvider";
-import AuthProvider from "@/providers/AuthProvider";
+import Menu from "@/components/global/Menu";
 
-export const metadata = {
-  title: "eclectic qbit",
-  description: "Interdipendence is the new indipendence",
-  icons: {
-    icon: "/icon.png",
-  },
-};
+import { HomepageCursor } from "@/components/cursors/Cursors";
+import LoadingScreen from "@/components/global/LoadingScreen";
+import Footer from "@/components/global/Footer";
+import CookiesPrompt from "@/components/global/CookiesPrompt";
+import FadePage from "@/components/specific/homeLayout/FadePage";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="min-h-screen min-w-screen bg-black text-white">
-      <body>
-        <LanguageProvider>
-          <MouseProvider>
-            <ScrollProvider>
-              <CurrentPageProvider>
-                <DebouncedResizeProvider>
-                  <ResizeProvider>
-                    <AuthProvider>{children}</AuthProvider>
-                  </ResizeProvider>
-                </DebouncedResizeProvider>
-              </CurrentPageProvider>
-            </ScrollProvider>
-          </MouseProvider>
-        </LanguageProvider>
-      </body>
-    </html>
+    <div className="w-full h-full cursor-none overflow-hidden">
+      <FadePage>
+        <HomepageCursor />
+        <LoadingScreen />
+        <Menu />
+        <div>
+          <div>{children}</div>
+        </div>
+        <CookiesPrompt />
+        <Footer />
+      </FadePage>
+    </div>
   );
 }
