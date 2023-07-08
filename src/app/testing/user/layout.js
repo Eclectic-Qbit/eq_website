ece78917d5215b9487a7bab6c64c2b4ae552592f
Enrypase <pasettoenrico02@gmail.com>
2023-07-08 11:58:41 +0200

    debug: ""

--- src/app/testing/user/layout.js
+++ src/app/testing/user/layout.js
@@ -1,24 +1,11 @@
 import Menu from "@/components/global/Menu";
 
-import { HomepageCursor } from "@/components/cursors/Cursors";
-import LoadingScreen from "@/components/global/LoadingScreen";
-import Footer from "@/components/global/Footer";
-import CookiesPrompt from "@/components/global/CookiesPrompt";
-import FadePage from "@/components/specific/homeLayout/FadePage";
-
 export default function RootLayout({ children }) {
   return (
     <div className="w-full h-full cursor-none overflow-hidden">
-      <FadePage>
-        <HomepageCursor />
-        <LoadingScreen />
-        <Menu />
-        <div>
-          <div>{children}</div>
-        </div>
-        <CookiesPrompt />
-        <Footer />
-      </FadePage>
+      <div>
+        <div>{children}</div>
+      </div>
     </div>
   );
 }
